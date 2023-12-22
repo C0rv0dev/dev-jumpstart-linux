61accb82b6d1a2eb2320a02f6d52308236c40dea
Lucas Couto <lucascoutojp@gmail.com>
2023-12-23 01:10:02 +0900

    added docker post install

--- installation_script.sh
+++ installation_script.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+# change these values to match your environment
 NODE_MAJOR=21
 LOG_PATH=~/.logs
+USERNAME="YOUR_USERNAME"
 
+# create my logs
 cd ~
 mkdir -p $LOG_PATH
-
-# create my logs 
-sudo touch $LOG_PATH/installation_log_error.txt \
+ 
+touch $LOG_PATH/installation_log_error.txt \
     $LOG_PATH/installation_log_success.txt
 
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt update
-sudo apt install -y \
+# Install Dependencies
+add-apt-repository ppa:ondrej/php -y
+apt update
+apt install -y \
+    git \
     cowsay \
     nodejs \
     npm \
@@ -20,50 +24,61 @@ sudo apt install -y \
     gnupg \
     mysql-server \
     apache2 \
+    php-cli \ 
+    php-mbstring \
+    php-curl \
+    unzip \
     php >> $LOG_PATH/installation_log_success.txt 2> $LOG_PATH/installation_log_error.txt
 
 # NPM and Nodejs Config
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
-sudo apt update
-sudo apt install -y nodejs >> $LOG_PATH/installation_log_success.txt 2> $LOG_PATH/installation_log_error.txt
+# Update already installed packages
+apt update
+apt install -y nodejs >> $LOG_PATH/installation_log_success.txt 2> $LOG_PATH/installation_log_error.txt
 
 # Docker Config
-sudo apt remove docker-desktop -y
+apt remove docker-desktop -y
     # remove if it exists
 rm -r $HOME/.docker/desktop
-sudo rm /usr/local/bin/com.docker.cli
-sudo apt purge docker-desktop -y
+rm /usr/local/bin/com.docker.cli
+apt purge docker-desktop -y
 
 # Add Docker's official GPG key:
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >> $LOG_PATH/installation_log_success.txt 2> $LOG_PATH/installation_log_error.txt
+# Install Docker Engine, containerd, and Docker Compose
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin >> $LOG_PATH/installation_log_success.txt 2> $LOG_PATH/installation_log_error.txt
 
+# Install Docker Desktop
 wget -O docker-desktop.deb "https://desktop.docker.com/linux/main/amd64/docker-desktop-4.26.1-amd64.deb?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64"
-sudo dpkg -i docker-desktop.deb >> $LOG_PATH/installation_log_success.txt 2> $LOG_PATH/installation_log_error.txt
+dpkg -i docker-desktop.deb >> $LOG_PATH/installation_log_success.txt 2> $LOG_PATH/installation_log_error.txt
 rm docker-desktop.deb
+
+# Docker Post Install Config
+groupadd docker
+usermod -aG docker $USERNAME
+newgrp docker
+chown "$USERNAME":"$USERNAME" /home/"$USERNAME"/.docker -R
+chmod g+rwx "$HOME/.docker" -R
 
 # Composer Config 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/compose
-
-# Yarn Config
-npm install -g yarn 
+mv composer.phar /usr/local/bin/compose
 
 echo "---------------------------------------------------------"
 cowsay "Installation Completed"
